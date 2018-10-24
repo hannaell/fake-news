02282e967502d2a8ffd18591b67470444343541f
hannaell <hannaell91@gmail.com>
2018-10-24 09:34:45 +0200

    Added css
    Added css using bootstrap

--- index.php
+++ index.php
@@ -6,26 +6,62 @@ declare(strict_types=1);
 
 require __DIR__.'/data.php';
 require __DIR__.'/functions.php';
-
-//$titles = getTitle($articles);
-
 ?>
-<!DOCTYPE html>
+
+<!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>Fake News</title>
-    </head>
-    <body>
-      <?php foreach ($articles as $post): ?>
-        <p><?php echo $post['title']; ?></p>
-        <p><?php echo $post['content']; ?></p>
-        <p><?php echo $post['author']; ?></p>
-        <p><?php echo $post['publishedDate']; ?></p>
-        <p><?php echo $post['likeCounter']; ?></p>
-      <?php endforeach; ?>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    </body>
+    <title>FakeNews</title>
+  </head>
+  <body>
+    <div class="jumbotron jumbotron-fluid"> <!-- Div1 -->
+      <div class="container"> <!-- Div2 -->
+        <h1 class="display-4">The Most Fake News Ever</h1>
+        <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+      </div> <!-- /Div2 -->
+    </div> <!-- /Div1 -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light"> <!-- Navbar -->
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav"> <!-- Div3 -->
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+        </ul>
+      </div> <!-- /Div3 -->
+    </nav> <!-- /Navbar -->
+
+    <?php foreach ($articles as $post): ?>
+      <h1><?php echo $post['title']; ?></h1>
+      <p><?php echo $post['content']; ?></p>
+      <p><?php echo $post['author']; ?></p>
+      <p><?php echo $post['publishedDate']; ?></p>
+      <p><?php echo $post['likeCounter']; ?></p>
+    <?php endforeach; ?>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
 </html>
